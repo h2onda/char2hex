2c8bd168a38612700058ab32171cbc65725e9d80
HONDA Hirofumi <h2onda@gmail.com>
2010-06-19 19:15:06 +0900

    add convert UCS expression and usage

--- char2hex.sh
+++ char2hex.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 #
-# Usage: char2hex.sh [STRINGS]
+# Usage: char2hex.sh [OPTIONS] [STRINGS]
 #
 # Convert strings to hex expression.
 #
 
-echo -n $* |hexdump -e '/1 "0x%02x, "'
-echo 0x00
+function to_hex()
+{
+	echo -n $* |hexdump -e '/1 "0x%02x, "'
+	echo 0x00
+}
+
+function to_ucs_charmaps()
+{
+	echo -n $*|iconv -t ucs-4le|hexdump -e '/4 "<U%04X>\n"'
+}
+
+function usage()
+{
+	cat <<-EOF
+Usage: $(basename $0) [OPTIONS] [STRINGS]
+
+  Convert strings to hex expression.
+
+  -c : convert to hex expression for char[] (default)
+  -u : convert to UCS expression for /usr/share/i18n/charmaps/*
+
+	EOF
+}
+
+while getopts "cu" flag "$@"; do
+	case $flag in
+		u) action=to_ucs_charmaps ;;
+		c) action=to_hex ;;
+		?|:) usage; exit ;;
+	esac
+done
+shift $(($OPTIND - 1))
+
+${action:=to_hex} $@
+
